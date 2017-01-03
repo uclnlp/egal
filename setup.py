f1c76ff98a6f8a893ba55c45472ccbce09445d1f
riedelcastro <sebastian.riedel@gmail.com>
2017-01-03 17:31:12 +0100

    setup script

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='egal',  # This is the name of your PyPI-package.
+    version='0.1',  # Update the version number for new releases
+    scripts=[]  # The name of your scipt, and also the command you'll be using for calling it
+)
