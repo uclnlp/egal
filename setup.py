12bd1a75d513263090922a6d5364a2d39d22efe8
riedelcastro <sebastian.riedel@gmail.com>
2017-01-03 17:58:03 +0100

    setup script improvement

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name='egal',  # This is the name of your PyPI-package.
     version='0.1',  # Update the version number for new releases
-    scripts=[]  # The name of your scipt, and also the command you'll be using for calling it
+    scripts=[],  # The name of your script, and also the command you'll be using for calling it
+    packages=['egal'],
+    author='Sebastian Riedel',
+    long_description=read('README.md'),
+    url='https://github.com/uclmr/egal',
+    author_email='sebastian.riedel@gmail.com',
+    include_package_data=True,
+    # package_data={'egal': ['egal/static']},
 )
